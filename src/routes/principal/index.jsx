41646b4b17f1c0a5c00ab8ca6a295a4f8421d4f9
meimei <1254630731@qq.com>
2019-09-20 14:58:46 +0800

    dev

--- src/routes/principal/index.jsx
+++ src/routes/principal/index.jsx
@@ -1,95 +1,96 @@
-import React, { Component } from "react";
+import React, { useState,useEffect } from "react";
 import "./css/index.css";
 import echarts from "echarts";
-class Principal extends Component {
-  componentDidMount() {
-    // 基于准备好的dom，初始化echarts实例
-    var myChart = echarts.init(document.getElementById("main"));
-    // 绘制图表
-    myChart.setOption({
-      title: {
-        text: "未来一周气温变化",
-        subtext: "纯属虚构"
-      },
-      tooltip: {
-        trigger: "axis"
-      },
-      legend: {
-        data: ["最高气温", "最低气温"]
-      },
-      toolbox: {
-        show: true,
-        feature: {
-          dataZoom: {
-            yAxisIndex: "none"
-          },
-          dataView: { readOnly: false },
-          magicType: { type: ["line", "bar"] },
-          restore: {},
-          saveAsImage: {}
-        }
-      },
-      xAxis: {
-        type: "category",
-        boundaryGap: false,
-        data: ["周一", "周二", "周三", "周四", "周五", "周六", "周日"]
-      },
-      yAxis: {
-        type: "value",
-        axisLabel: {
-          formatter: "{value} °C"
-        }
-      },
-      series: [
-        {
-          name: "最高气温",
-          type: "line",
-          data: [11, 11, 15, 13, 12, 13, 10],
-          markPoint: {
-            data: [
-              { type: "max", name: "最大值" },
-              { type: "min", name: "最小值" }
-            ]
-          },
-          markLine: {
-            data: [{ type: "average", name: "平均值" }]
+function Principal(){
+  let [option,changeOption]=useState([])
+  let [,changeOption]=useState([])
+  useEffect(()=>{
+    {
+      var myChart = echarts.init(document.getElementById("main"));
+      myChart.setOption({
+        title: {
+          text: "xx同学的日周考成绩统计表",
+          subtext: "仅供参考"
+        },
+        tooltip: {
+          trigger: "axis"
+        },
+        legend: {
+          data: ["最高气温", "最低气温"]
+        },
+        toolbox: {
+          show: true,
+          feature: {
+            dataZoom: {
+              yAxisIndex: "none"
+            },
+            dataView: { readOnly: false },
+            magicType: { type: ["line", "bar"] },
+            restore: {},
+            saveAsImage: {}
           }
         },
-        {
-          name: "最低气温",
-          type: "line",
-          data: [1, -2, 2, 5, 3, 2, 0],
-          markPoint: {
-            data: [{ name: "周最低", value: -2, xAxis: 1, yAxis: -1.5 }]
-          },
-          markLine: {
-            data: [
-              { type: "average", name: "平均值" },
-              [
-                {
-                  symbol: "none",
-                  x: "90%",
-                  yAxis: "max"
-                },
-                {
-                  symbol: "circle",
-                  label: {
-                    normal: {
-                      position: "start",
-                      formatter: "最大值"
-                    }
-                  },
-                  type: "max",
-                  name: "最高点"
-                }
-              ]
-            ]
+        xAxis: {
+          type: "category",
+          boundaryGap: false,
+          data: ["周一", "周二", "周三", "周四", "周五", "周六", "周日"]
+        },
+        yAxis: {
+          type: "value",
+          axisLabel: {
+            formatter: "{value} °C"
           }
-        }
-      ]
-    });
-  }
-  render() {
+        },
+        series: [
+          {
+            name: "最高气温",
+            type: "line",
+            data: [11, 11, 15, 13, 12, 13, 10],
+            markPoint: {
+              data: [
+                { type: "max", name: "最大值" },
+                { type: "min", name: "最小值" }
+              ]
+            },
+            markLine: {
+              data: [{ type: "average", name: "平均值" }]
+            }
+          },
+          {
+            name: "最低气温",
+            type: "line",
+            data: [1, -2, 2, 5, 3, 2, 0],
+            markPoint: {
+              data: [{ name: "周最低", value: -2, xAxis: 1, yAxis: -1.5 }]
+            },
+            markLine: {
+              data: [
+                { type: "average", name: "平均值" },
+                [
+                  {
+                    symbol: "none",
+                    x: "90%",
+                    yAxis: "max"
+                  },
+                  {
+                    symbol: "circle",
+                    label: {
+                      normal: {
+                        position: "start",
+                        formatter: "最大值"
+                      }
+                    },
+                    type: "max",
+                    name: "最高点"
+                  }
+                ]
+              ]
+            }
+          }
+        ]
+      });
+    }
+  },[option]) 
     return (
       <div className="warper">
         <header>重点关注学生考试成绩统计图</header>
@@ -184,6 +185,6 @@ class Principal extends Component {
         </div>
       </div>
     );
-  }
+  
 }
 export default Principal;

5b279ddab5cb6133ed123a530c22d27c57d57b89
dawei126156 <dawei126156@163.com>
2019-09-20 14:37:00 +0800

    login

--- src/router.js
+++ src/router.js
@@ -1,12 +1,13 @@
 import React from 'react';
-import { Router, Route, Switch } from 'dva/router';
-import IndexPage from './routes/IndexPage';
+import { Router, Route, Switch ,Redirect} from 'dva/router';
+import Login from "./routes/login";
 
 function RouterConfig({ history }) {
   return (
     <Router history={history}>
       <Switch>
-        <Route path="/" exact component={IndexPage} />
+      <Route path="/login" exact component={Login} />
+        <Redirect from='/' to='/login'/>
       </Switch>
     </Router>
   );

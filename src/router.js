9dc74d75876593ca99b95b29faf69e59a2918e38
meimei <1254630731@qq.com>
2019-09-20 14:10:25 +0800

    xiaozhang

--- src/router.js
+++ src/router.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Router, Route, Switch } from 'dva/router';
 import IndexPage from './routes/IndexPage';
-
+import Principal from "./routes/principal"
 function RouterConfig({ history }) {
   return (
     <Router history={history}>
       <Switch>
         <Route path="/" exact component={IndexPage} />
+        <Route path="/principal" component={Principal}></Route>
       </Switch>
     </Router>
   );
--- src/routes/IndexPage.js
+++ src/routes/IndexPage.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { connect } from 'dva';
 import styles from './IndexPage.css';
-
 function IndexPage() {
   return (
     <div className={styles.normal}>
